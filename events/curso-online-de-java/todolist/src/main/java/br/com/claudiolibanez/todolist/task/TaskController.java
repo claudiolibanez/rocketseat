c83931554aa1e7b14b3da1865dbb9f8ba668dc92
Claudio Soares <claudiolibanez@gmail.com>
2023-10-15 22:54:24 -0300

    new rocketseat project

---
+++ events/curso-online-de-java/todolist/src/main/java/br/com/claudiolibanez/todolist/task/TaskController.java
@@ -0,0 +1,98 @@
+package br.com.claudiolibanez.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.claudiolibanez.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+  
+  @Autowired
+  private ITaskRepository taskRepository;
+
+  @GetMapping("/")
+  public List<TaskModel> list(
+    HttpServletRequest request
+  ) {
+    var idUser = request.getAttribute("idUser");
+
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+
+    return tasks;
+  }
+
+  @PostMapping("/")
+  public ResponseEntity create(
+    @RequestBody TaskModel taskModel,
+    HttpServletRequest request
+  ) {
+    var idUser = request.getAttribute("idUser");
+    taskModel.setIdUser((UUID) idUser);
+
+    var currentData = LocalDateTime.now();
+
+    if (currentData.isAfter(taskModel.getStartAt()) || currentData.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body("The start date / end date cannot be less than the current date");
+    }
+
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body("The start date cannot be greater than the end date");
+    }
+
+    var taskCreated = this.taskRepository.save(taskModel);
+
+    return ResponseEntity
+      .status(HttpStatus.CREATED)
+      .body(taskCreated);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity update(
+    @RequestBody TaskModel taskModel,
+    @PathVariable("id") UUID id,
+    HttpServletRequest request
+  ) {
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body("Task not found");
+    }
+
+    var idUser = request.getAttribute("idUser");
+
+    if (!task.getIdUser().equals(idUser)) {
+      return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body("User does not have permission to change this task");
+    }
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    var taskUpdated = this.taskRepository.save(task);
+
+    return ResponseEntity
+      .ok()
+      .body(taskUpdated);
+  }
+}
